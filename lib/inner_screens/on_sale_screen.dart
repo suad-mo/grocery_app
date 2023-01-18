461d2be54251ba86e24f3a707c40aa6ea67c7c14
Suad Krvavac <suad.krvavac@gmail.com>
2023-01-18 09:58:38 +0100

    Handle UI if there is no products on sale. (Create new UI to it)...

--- lib/inner_screens/on_sale_screen.dart
+++ lib/inner_screens/on_sale_screen.dart
@@ -11,6 +11,7 @@ class OnSaleScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool _isEmpty = false;
     final Utils utils = Utils(context);
     final Color color = utils.color;
     Size size = utils.getScreenSize;
@@ -35,15 +36,38 @@ class OnSaleScreen extends StatelessWidget {
           isTitle: true,
         ),
       ),
-      body: GridView.count(
-        // shrinkWrap: true,
-        // physics: const NeverScrollableScrollPhysics(),
-        crossAxisCount: 2,
-        padding: EdgeInsets.zero,
-        // crossAxisSpacing: 10,
-        childAspectRatio: size.width / (size.height * 0.45),
-        children: List.generate(16, (index) => const OnSaleWidget()),
-      ),
+      body: _isEmpty
+          ? Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: Image.asset('assets/images/box.png'),
+                    ),
+                    Text(
+                      'No products on sale yet, \nStay tuned',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: color,
+                        fontSize: 30,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          : GridView.count(
+              // shrinkWrap: true,
+              // physics: const NeverScrollableScrollPhysics(),
+              crossAxisCount: 2,
+              padding: EdgeInsets.zero,
+              // crossAxisSpacing: 10,
+              childAspectRatio: size.width / (size.height * 0.45),
+              children: List.generate(16, (index) => const OnSaleWidget()),
+            ),
     );
   }
 }
