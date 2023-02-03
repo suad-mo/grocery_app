7d0b0a554b94e65aff8b4145f5ee2c17e2582e1d
Suad Krvavac <suad.krvavac@gmail.com>
2023-02-03 11:54:29 +0100

    Finish the wishlist state management...

--- lib/screens/wishlist/wishlist_screen.dart
+++ lib/screens/wishlist/wishlist_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:grocery_app/providers/wishlist_provider.dart';
 import 'package:grocery_app/screens/wishlist/wishlist_widget.dart';
 import 'package:grocery_app/widgets/back_widget.dart';
 import 'package:grocery_app/widgets/empty_screen.dart';
 import 'package:grocery_app/widgets/text_widget.dart';
+import 'package:provider/provider.dart';
 
 import '../../services/global_methods.dart';
 import '../../services/utils.dart';
@@ -17,8 +19,10 @@ class WishlistScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = Utils(context).getScreenSize;
     final Color color = Utils(context).color;
-    bool _isEmpty = true;
-    return _isEmpty
+    final wishlistProvider = Provider.of<WishlistProvider>(context);
+    final wishlistItemsList =
+        wishlistProvider.getWishlistItems.values.toList().reversed.toList();
+    return wishlistItemsList.isEmpty
         ? const EmptyScreen(
             title: 'Your Wishlist Is Empty',
             subtitle: 'Explore more and shortlist some items',
@@ -33,7 +37,7 @@ class WishlistScreen extends StatelessWidget {
               elevation: 0,
               backgroundColor: Theme.of(context).scaffoldBackgroundColor,
               title: TextWidget(
-                text: 'Wishlist (2)',
+                text: 'Wishlist (${wishlistItemsList.length})',
                 color: color,
                 textSize: 22,
                 isTitle: true,
@@ -44,7 +48,12 @@ class WishlistScreen extends StatelessWidget {
                     GlobalMethod.warningDialog(
                       title: 'Empty your wishlist?',
                       subtitle: 'Are your sure??',
-                      fct: () {},
+                      fct: () {
+                        wishlistProvider.clearWishlist();
+                        if (Navigator.canPop(context)) {
+                          Navigator.pop(context);
+                        }
+                      },
                       context: context,
                     );
                   },
@@ -56,11 +65,15 @@ class WishlistScreen extends StatelessWidget {
               ],
             ),
             body: MasonryGridView.count(
+              itemCount: wishlistItemsList.length,
               crossAxisCount: 2,
               // mainAxisSpacing: 16,
               // crossAxisSpacing: 20,
               itemBuilder: (context, index) {
-                return const WishlistWidget();
+                return ChangeNotifierProvider.value(
+                  value: wishlistItemsList[index],
+                  child: const WishlistWidget(),
+                );
               },
             ),
           );
