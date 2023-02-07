4bfcc56000647f195f4c63708de8e5692038f9d5
Suad Krvavac <suad.krvavac@gmail.com>
2023-02-07 10:24:55 +0100

    Create a loading manager widget...

---
+++ lib/screens/loading_manager.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class LoadingManager extends StatelessWidget {
+  const LoadingManager({
+    super.key,
+    required this.isLoading,
+    required this.child,
+  });
+
+  final bool isLoading;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        child,
+        isLoading
+            ? Container(
+                color: Colors.black.withOpacity(0.7),
+              )
+            : Container(),
+        isLoading
+            ? const Center(
+                child: CircularProgressIndicator(color: Colors.white),
+              )
+            : Container()
+      ],
+    );
+  }
+}
