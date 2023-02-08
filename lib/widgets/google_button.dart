d6c522220adb9846834e2af3534b230b3ad8163b
Suad Krvavac <suad.krvavac@gmail.com>
2023-02-08 10:27:14 +0100

    Implement the google sign method (Allow the user to login using his gmail acc...

--- lib/widgets/google_button.dart
+++ lib/widgets/google_button.dart
@@ -1,15 +1,48 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:grocery_app/consts/firebase_const.dart';
+import '../screens/btm_bar.dart';
+import '../services/global_methods.dart';
 import 'text_widget.dart';
 
 class GoogleButton extends StatelessWidget {
   const GoogleButton({Key? key}) : super(key: key);
+
+  Future<void> _googleSignIn(BuildContext context) async {
+    final googleSignIn = GoogleSignIn();
+    final googleAccount = await googleSignIn.signIn();
+    if (googleAccount != null) {
+      final googleAuth = await googleAccount.authentication;
+      if (googleAuth.accessToken != null && googleAuth.idToken != null) {
+        try {
+          await authInstance.signInWithCredential(
+            GoogleAuthProvider.credential(
+              idToken: googleAuth.idToken,
+              accessToken: googleAuth.accessToken,
+            ),
+          );
+          Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (context) => const BottomBarScreen(),
+          ));
+        } on FirebaseAuthException catch (error) {
+          GlobalMethod.errorDialog(
+              subtitle: '${error.message}', context: context);
+        } catch (error) {
+          GlobalMethod.errorDialog(subtitle: '$error', context: context);
+        } finally {}
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.blue,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          _googleSignIn(context);
+        },
         child: Row(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
